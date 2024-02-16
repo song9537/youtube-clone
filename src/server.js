ab5ec11a660303b46a2efb98dcd8edbfaa14bd7b
song9537 <140922961+song9537@users.noreply.github.com>
2024-02-16 14:37:02 -0400

    Initial setup with basic server on port 4000

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
