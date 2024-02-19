e748a4a24ade1003b624134399a28872192e30a5
song9537 <140922961+song9537@users.noreply.github.com>
2024-02-19 17:35:04 -0400

    Added more basic server setups

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const home = (req, res) => {
+  return res.send("hello");
+};
 
-app.get("/", handleHome);
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
