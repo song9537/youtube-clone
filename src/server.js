08afddceadac63b68c1616afa098861a5bf8b92b
song9537 <140922961+song9537@users.noreply.github.com>
2024-06-25 22:56:07 -0300

    creating routers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -15,8 +18,9 @@ const login = (req, res) => {
 };
 
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
