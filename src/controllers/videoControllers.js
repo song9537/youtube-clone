246be1d0aaa8cd77e3ccc05625e1bdcb7ec8e013
song9537 <140922961+song9537@users.noreply.github.com>
2024-07-27 19:24:51 -0300

    Search tab created but to be upgraded in future

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.render("server-error", { error });
@@ -68,4 +68,17 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { join, login } from "../controllers/userControllers";
-import { home } from "../controllers/videoControllers";
+import { home, search } from "../controllers/videoControllers";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
